c142ce27da0d46c5e11753658dfdf5c6cd595740
vineetha19 <42667307+vineetha19@users.noreply.github.com>
2018-10-08 04:03:07 -0700

    broadcast

---
+++ app/src/main/java/com/example/admin/broadcast/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.admin.broadcast;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    MyReceiver receiver;
+    IntentFilter intentFilter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        receiver = new MyReceiver();
+        intentFilter = new IntentFilter("com.journaldev.broadcastreceiver.SOME_ACTION");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(receiver);
+    }
+
+    @OnClick(R.id.button)
+    void someMethod() {
+
+        Intent intent = new Intent("com.journaldev.broadcastreceiver.SOME_ACTION");
+        sendBroadcast(intent);
+    }
+}
